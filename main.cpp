956a70e88bcd2949bbdab57f1b4f18819bf29485
fxgn123 <80616298+fxgn123@users.noreply.github.com>
2021-03-18 19:31:55 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,43 +1,54 @@
 #include<stdio.h>
+#include<malloc.h>
 
-#define Inf 9999
-void Floyd(int map[4][4]);
+#define MAX_VERTEX_NUM 10
+
+typedef struct {//定义图的数据结构
+	int edges[MAX_VERTEX_NUM][MAX_VERTEX_NUM];//矩阵用于存储权重
+	int vexnum;//节点数
+	int edgenum;//边数
+}MGraph;
+void kruskal(MGraph* G);
 void readin();
 
 int main() {
 	readin();
 	return 0;
 }
-
 void readin() {
-	int map[4][4];
-	map[0][0] = 0;   map[0][1] = 2;   map[0][2] = 6; map[0][3] = 4;
-	map[1][0] = Inf; map[1][1] = 0;   map[1][2] = 3; map[1][3] = Inf;
-	map[2][0] = 7;   map[2][1] = Inf; map[2][2] = 0; map[2][3] = 1;
-	map[3][0] = 5;   map[3][1] = Inf; map[3][2] = 1; map[3][3] = 0;
-	Floyd(map);
+	MGraph* mgraph = (MGraph*)malloc(sizeof(MGraph));
+	int i = -1;
+	int START;
+	printf("请输入有几个节点，有几条边:");
+	scanf("%d %d", &mgraph->vexnum, &mgraph->edgenum);
+	printf("请输入图的邻接矩阵:\n");
+	for (int a = 0; a < mgraph->vexnum; a++) {
+		for (int b = 0; b < mgraph->vexnum; b++) {
+			scanf("%d", &mgraph->edges[a][b]);
+		}
+	}
+	printf("请输入从哪个节点开始生成最小生成树:");
+	scanf("%d", &START);
+	kruskal(mgraph);
 }
-void Floyd(int map[4][4]) {
-	for (int a = 0; a < 4; a++) {
-		for (int b = 0; b < 4; b++) {
-			for (int c = 0; c < 4; c++) {
-				if ((map[b][a] + map[a][c]) < map[b][c]) {
-					map[b][c] = map[b][a] + map[a][c];
+void kruskal(MGraph* G) {
+	int min;
+	int signa, signb;
+	int visited[MAX_VERTEX_NUM] = { 0 };//保存已经存的节点
+	for (int x = 0; x < G->vexnum - 1; x++) {
+		min = 100;
+		for (int a = 0; a < G->vexnum; a++) {
+			for (int b = 0; b < G->vexnum; b++) {
+				if ((G->edges[a][b] <= min) && (!(visited[a] && visited[b]))&& (G->edges[a][b] != 0)) {
+					min = G->edges[a][b];
+					signa = a;
+					signb = b;
 				}
 			}
 		}
-		
+		visited[signa] = 1;
+		visited[signb] = 1;
+		printf("节点:%d 节点:%d 权重:%d\n", signa, signb, G->edges[signa][signb]);
 	}
-	for (int i = 0; i < 4; i++) {
-		for (int j = 0; j < 4; j++) {
-			if (map[i][j] != Inf) {
-				printf("%d ", map[i][j]);
-			}
-			else {
-				printf("�޷�����");
-			}
-
-		}
-		printf("\n");
-	}
+	
 }
