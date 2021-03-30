915200c7ee90ccf13b8bf45fce47f220091ae55e
fxgn123 <80616298+fxgn123@users.noreply.github.com>
2021-03-30 19:00:48 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,43 +1,54 @@
 #include<stdio.h>
 
-#define Inf 9999
-void Floyd(int map[4][4]);
 void readin();
-
+void mergeset(int *T, int p, int r);
+void merge(int *T,int p, int r);
 int main() {
+
 	readin();
 	return 0;
 }
 
 void readin() {
-	int map[4][4];
-	map[0][0] = 0;   map[0][1] = 2;   map[0][2] = 6; map[0][3] = 4;
-	map[1][0] = Inf; map[1][1] = 0;   map[1][2] = 3; map[1][3] = Inf;
-	map[2][0] = 7;   map[2][1] = Inf; map[2][2] = 0; map[2][3] = 1;
-	map[3][0] = 5;   map[3][1] = Inf; map[3][2] = 1; map[3][3] = 0;
-	Floyd(map);
+	printf("请输入需要排序的数组:");
+	int T[100];
+	int i = 0;
+	int x = 0;
+	while (1) {
+		scanf("%d", &x);
+		char s = getchar();
+		T[i] = x;
+		i++;
+		if (s == '\n')break;
+	}
+	mergeset(T, 0, --i);
+	for (int a = 0; a <=i; a++) {
+		printf("%d ", T[a]);
+	}
 }
-void Floyd(int map[4][4]) {
-	for (int a = 0; a < 4; a++) {
-		for (int b = 0; b < 4; b++) {
-			for (int c = 0; c < 4; c++) {
-				if ((map[b][a] + map[a][c]) < map[b][c]) {
-					map[b][c] = map[b][a] + map[a][c];
-				}
-			}
+void mergeset(int *T, int p, int r) {
+	if (p >= r) return;
+	int mid = (p + r) / 2;
+	mergeset(T, p, mid);
+	mergeset(T,mid+1,r);
+	merge(T, p, r);
+}
+void merge(int *T,int p, int r) {
+	int t[100];
+	int mid = (p + r) / 2;
+	int i = p;
+	int j = mid + 1;
+	int a = p;
+	while ((i <= mid) && (j <= r)) {
+		if (T[i] > T[j])t[a++] = T[j++];
+		else {
+			t[a++] = T[i++];
 		}
 		
 	}
-	for (int i = 0; i < 4; i++) {
-		for (int j = 0; j < 4; j++) {
-			if (map[i][j] != Inf) {
-				printf("%d ", map[i][j]);
-			}
-			else {
-				printf("无法到达");
-			}
-
-		}
-		printf("\n");
+	while (i <= mid)t[a++] = T[i++];
+	while (j <= r)t[a++] = T[j++];
+	for (int b = p; b <= r; b++) {
+		T[b] = t[b];
 	}
 }
