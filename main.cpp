e1c4041c2ed7930d95f82222037267246937fe7b
fxgn123 <80616298+fxgn123@users.noreply.github.com>
2021-06-01 13:29:53 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,43 +1,60 @@
-#include<stdio.h>
+#include <stdio.h>
+int color[100];
+bool OK(int k, int c[][100]) {           
+	int i;
+	for (i = 1; i < k; i++)
+		if (c[k][i] == 1 && color[i] == color[k])
+			return false;
+	return true;
+}
 
-#define Inf 9999
-void Floyd(int map[4][4]);
-void readin();
+void graphcolor(int n, int m, int c[][100]) {
+	int i, k;
+	int flag = 0;
+	for (i = 1; i <= n; i++) {
+		color[i] = 0;                     
+	}
+	k = 1;
+	while (k >= 1) {
+		color[k] = color[k] + 1;
+		while (color[k] <= m) {
+			if (OK(k, c))
+				break;
+			else
+				color[k] = color[k] + 1;      
+		}
+		if (color[k] <= m && k == n) {         
+			for (i = 1; i <= n; i++) {
+				printf("%d ", color[i]);
+			}
+			printf("\n");
+			flag = 1;
+		}
+		else if (color[k] <= m && k < n) {
+			k = k + 1;                       
+		}
+		else {
+			color[k] = 0;
+			k = k - 1;                      
+		}
+	}
+	if (flag == 1) {
+		printf("NO\n");
+	}
+}
 
 int main() {
-	readin();
+	int i, j, n, m;
+	int c[100][100];
+	printf("请输入顶点数n和着色数m：\n");
+	scanf("%d%d", &n, &m);
+	printf("请输入无向图的邻接矩阵：\n");
+	for (i = 1; i <= n; i++) {
+		for (j = 1; j <= n; j++) {
+			scanf("%d", &c[i][j]);
+		}
+	}
+	printf("着色可能解m：\n");
+	graphcolor(n, m, c);
 	return 0;
-}
-
-void readin() {
-	int map[4][4];
-	map[0][0] = 0;   map[0][1] = 2;   map[0][2] = 6; map[0][3] = 4;
-	map[1][0] = Inf; map[1][1] = 0;   map[1][2] = 3; map[1][3] = Inf;
-	map[2][0] = 7;   map[2][1] = Inf; map[2][2] = 0; map[2][3] = 1;
-	map[3][0] = 5;   map[3][1] = Inf; map[3][2] = 1; map[3][3] = 0;
-	Floyd(map);
-}
-void Floyd(int map[4][4]) {
-	for (int a = 0; a < 4; a++) {
-		for (int b = 0; b < 4; b++) {
-			for (int c = 0; c < 4; c++) {
-				if ((map[b][a] + map[a][c]) < map[b][c]) {
-					map[b][c] = map[b][a] + map[a][c];
-				}
-			}
-		}
-		
-	}
-	for (int i = 0; i < 4; i++) {
-		for (int j = 0; j < 4; j++) {
-			if (map[i][j] != Inf) {
-				printf("%d ", map[i][j]);
-			}
-			else {
-				printf("无法到达");
-			}
-
-		}
-		printf("\n");
-	}
 }
